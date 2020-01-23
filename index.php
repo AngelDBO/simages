9cb39baedd4d09c586ccfa60e645bdf56b3f09e5
angel <angel@ACER-ANGEL>
2020-01-22 23:00:58 -0500

    2020-01-22 22:53:29

--- index.php
+++ index.php
@@ -5,18 +5,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Simages | Log in</title>
-        <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <link rel="stylesheet" href="dist/adminlte.min.css">
-
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
-
-        <!-- Google Font -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
         <script src="dist/adminlte.min.js"></script>
@@ -28,9 +18,8 @@
                 <a href="#">
                     <b>Sima</b>ges</a>
             </div>
-            <!-- /.login-logo -->
+
             <div class="login-box-body">
-               <!-- <p class="login-box-msg">Sign in to start your session</p> -->
 
                 <form id="form-login">
                     <div class="form-group has-feedback">
@@ -42,19 +31,11 @@
                         <span class="glyphicon glyphicon-lock form-control-feedback" id="pass"></span>
                     </div>
                     <div class="row">
-                        <!-- <div class="col-xs-8">
 
-                             <label class="checkbox-group">
-                                 <input type="checkbox"> Remember me
-                                 <span class="checkmark"></span>
-                             </label>
-
-                         </div> -->
-                        <!-- /.col -->
                         <div class="col-xs-4">
                             <input type="button" value="Iniciar" id="btnloguear"class="btn btn-primary btn-block btn-flat">
                         </div>
-                        <!-- /.col -->
+
                     </div>
                 </form>
                 <br>
@@ -64,9 +45,9 @@
                 <a href="#" class="text-center">Register a new membership</a>
 
             </div>
-            <!-- /.login-box-body -->
+
         </div>
-        <!-- /.login-box -->
+
     </body>
 
     <?php require 'scripts/scripts.php'; ?>
