33959ac5cbdaac171fe8f780703571d46058e1f5
angel <angel@ACER-ANGEL>
2020-01-22 23:48:09 -0500

    2020-01-22 23:48:29

--- vista/registrar.php
+++ vista/registrar.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+
 <html>
 
     <head>
@@ -6,60 +6,66 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Simages | Log in</title>
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <link rel="stylesheet" href="dist/adminlte.min.css">
+        <link rel="stylesheet" href="../dist/adminlte.min.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
-        <script src="dist/adminlte.min.js"></script>
+        <script src="../dist/adminlte.min.js"></script>
     </head>
 
     <body class="hold-transition login-page">
         <div class="login-box">
             <div class="login-logo">
-
-                <b>Sima</b>ges</a>
+                <a href="../index.php">
+                    <b>Sima</b>ges</a>
             </div>
 
             <div class="login-box-body">
-
-                <form id="form-login">
+                <h3 style="text-align: center;">Formulario de registro</h3>
+                <br>
+                <form id="formularioRegistro">
                     <div class="form-group has-feedback">
-                        <input type="email" class="form-control" placeholder="" id="email">
-                        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+
+                        <input type="email" class="form-control" placeholder="Ingrese su usuario" id="usuario">
+                        <span class="glyphicon glyphicon-user form-control-feedback"></span>
                     </div>
                     <div class="form-group has-feedback">
-                        <input type="password" class="form-control" placeholder="">
+
+                        <input type="password" class="form-control" placeholder="Ingrese su contraseña">
                         <span class="glyphicon glyphicon-lock form-control-feedback" id="pass"></span>
                     </div>
+                    <div class="form-group has-feedback">
+
+                        <input type="text" class="form-control" id="email" placeholder="Ingrese su email" >
+                        <span class="glyphicon glyphicon-inbox form-control-feedback" ></span>
+                    </div>
+
                     <div class="row">
 
                         <div class="col-xs-4">
-                            <input type="button" value="Iniciar" id="btnloguear"class="btn btn-primary btn-block btn-flat">
+                            <input type="button" value="Registrar" id="btnregistrar"class="btn btn-primary btn-block ">
                         </div>
 
                     </div>
                 </form>
-                <br>
-                <a href="#">Olvide mi contraseña</a>
-                <br>
-                <br>
-                <a href="vista/registrar.php" class="text-center">Registar un nuevo usuario</a>
-
             </div>
 
         </div>
 
     </body>
 
-    <?php require 'scripts/scripts.php'; ?>
+    <?php require '../scripts/scripts.php'; ?>
 
     <script type="text/javascript">
         $(document).ready(function() {
-            $('#btnloguear').click(function() {
-                if ($('#email').val() == "") {
-                    alertify.alert("Debes ingresar tu email");
+            $('#btnregistrar').click(function() {
+                if ($('#usuario').val() == "") {
+                    alertify.alert("Debes ingresar su usuario");
                     return false;
                 } else if ($('#pass').val() == "") {
                     alertify.alert("Debes ingresar contraseña");
+                    return false;
+                } else if ($('#email') == "") {
+                    alertify.alert("Debes ingresar tu correo");
                     return false;
                 }
             });
